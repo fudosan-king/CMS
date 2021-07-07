aa9a2f040cccecac312f80e7076a7477f0fa1c24
Truong Tam <tam@propolife.co.jp>
2021-07-07 11:49:54 +0700

    Create edit tab photos

--- logknot/dashboard/models.py
+++ logknot/dashboard/models.py
@@ -5,9 +5,9 @@ from django.core.validators import *  # noqa
 
 
 class Photos(EmbeddedDocument):
-    url = fields.StringField(max_length=255)
-    category = fields.StringField(max_length=255)
-    comment = fields.StringField(max_length=255)
+    path = fields.StringField(max_length=255, blank=True)
+    category = fields.StringField(max_length=255, blank=True)
+    comment = fields.StringField(max_length=255, blank=True)
 
 
 class Buildings(Document):
@@ -15,18 +15,18 @@ class Buildings(Document):
         default=datetime.datetime.now, editable=False,
     )
     building_name = fields.StringField(max_length=255)
-    removed = fields.BooleanField(default=False)
-    create_by = fields.StringField(max_length=255, default='system')
-    last_time_remove = fields.DateTimeField(default=datetime.datetime.now)
-    remove_by = fields.StringField(max_length=255, default='system')
-    last_time_update = fields.DateTimeField(default=datetime.datetime.now)
-    update_by = fields.StringField(max_length=255, default='system')
-    last_time_rollback = fields.DateTimeField(default=datetime.datetime.now)
-    rollback_by = fields.StringField(max_length=255, default='system')
+    removed = fields.BooleanField(default=False, blank=True)
+    create_by = fields.StringField(max_length=255, default='system', blank=True)
+    last_time_remove = fields.DateTimeField(default=datetime.datetime.now, blank=True)
+    remove_by = fields.StringField(max_length=255, default='system', blank=True)
+    last_time_update = fields.DateTimeField(default=datetime.datetime.now, blank=True)
+    update_by = fields.StringField(max_length=255, default='system', blank=True)
+    last_time_rollback = fields.DateTimeField(default=datetime.datetime.now, blank=True)
+    rollback_by = fields.StringField(max_length=255, default='system', blank=True)
     photos = fields.ListField(
         fields.EmbeddedDocumentField('Photos'),
         blank=True,
-        default=[],
+        default=[]
     )
 
     @staticmethod
@@ -48,11 +48,19 @@ class Buildings(Document):
             self.building_name = request.POST.get('building_name', None)
         self.last_time_update = datetime.datetime.now
         self.update_by = str(request.user)
-        photos = Photos()
-        photos.url = 'aaaa'
-        photos.category = 'bbbb'
-        photos.comment = 'cccc'
-        self.photos.append(photos)
+        self.photos = []
+        path = request.POST.getlist('path', [])
+        category = request.POST.getlist('category', [])
+        comment = request.POST.getlist('comment', [])
+        if path:
+            for i in range(0, len(path)):
+                photos = Photos()
+                photos.path = path[i]
+                if category and len(category):
+                    photos.category = category[i]
+                if comment and len(comment):
+                    photos.comment = comment[i]
+                self.photos.append(photos)
         return self
 
     def remove(self, request):

fcd0e02f294c249ca42fbf0b83513550f90b236d
Truong Tam <tam@propolife.co.jp>
2021-07-12 17:05:37 +0700

    Limit pref

--- logknot/home/management/commands/locations.py
+++ logknot/home/management/commands/locations.py
@@ -14,6 +14,57 @@ AZA_FIELD = 22
 AZA_FIELD_KANA = 13
 IGNORE_CODE = []
 
+PREF_MAP = [
+    # None,
+    # u'北海道',
+    # u'青森県',
+    # u'岩手県',
+    # u'宮城県',
+    # u'秋田県',
+    # u'山形県',
+    # u'福島県',
+    # u'茨城県',
+    # u'栃木県',
+    # u'群馬県',
+    u'埼玉県',
+    u'千葉県',
+    u'東京都',
+    # u'神奈川県',
+    # u'新潟県',
+    # u'富山県',
+    # u'石川県',
+    # u'福井県',
+    # u'山梨県',
+    # u'長野県',
+    # u'岐阜県',
+    # u'静岡県',
+    # u'愛知県',
+    # u'三重県',
+    # u'滋賀県',
+    # u'京都府',
+    # u'大阪府',
+    # u'兵庫県',
+    # u'奈良県',
+    # u'和歌山県',
+    # u'鳥取県',
+    # u'島根県',
+    # u'岡山県',
+    # u'広島県',
+    # u'山口県',
+    # u'徳島県',
+    # u'香川県',
+    # u'愛媛県',
+    # u'高知県',
+    # u'福岡県',
+    # u'佐賀県',
+    # u'長崎県',
+    # u'熊本県',
+    # u'大分県',
+    # u'宮崎県',
+    # u'鹿児島県',
+    # u'沖縄県'
+]
+
 # Example:
 # https://fudosan-king.jp/api/area/%E6%9D%B1%E4%BA%AC%E9%83%BD/%E4%B8%89%E9%B7%B9%E5%B8%82/%E4%B8%8A%E9%80%A3%E9%9B%80
 
@@ -128,7 +179,8 @@ class Command(BaseCommand):
         if data:
             locations = {}
             for root_code in data:
-                locations[data[root_code]['pref']] = data[root_code]['prefdata']
+                if data[root_code]['pref'] in PREF_MAP:
+                    locations[data[root_code]['pref']] = data[root_code]['prefdata']
 
             outfile = open('data/locations.json', 'wb')
             outfile = codecs.lookup('utf-8')[-1](outfile)

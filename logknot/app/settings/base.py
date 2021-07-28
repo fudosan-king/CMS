7e8b633fbd91f3b7ea00cdf94d8bb1b9ef656f4b
Truong Tam <tam@propolife.co.jp>
2021-07-28 16:45:33 +0700

    Mode production

--- logknot/app/settings/base.py
+++ logknot/app/settings/base.py
@@ -173,7 +173,7 @@ STATICFILES_DIRS = [
 # ManifestStaticFilesStorage is recommended in production, to prevent outdated
 # JavaScript / CSS assets being served from cache (e.g. after a Wagtail upgrade).
 # See https://docs.djangoproject.com/en/3.2/ref/contrib/staticfiles/#manifeststaticfilesstorage
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
@@ -190,7 +190,7 @@ WAGTAIL_SITE_NAME = "logknot"
 
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
-BASE_URL = 'http://example.com'
+BASE_URL = 'http://cms.localhost'
 
 WAGTAILIMAGES_IMAGE_MODEL = 'home.BuildingImage'
 

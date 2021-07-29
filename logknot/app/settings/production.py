f985d13817f6fcfbce15c463e49498f5d2e73de2
Truong Tam <tam@propolife.co.jp>
2021-07-29 17:09:05 +0700

    Update config

--- logknot/app/settings/production.py
+++ logknot/app/settings/production.py
@@ -1,7 +1,6 @@
 from .base import *  # noqa
 
-# DEBUG_PROPAGATE_EXCEPTIONS = True
-DEBUG = False
+DEBUG_PROPAGATE_EXCEPTIONS = True
 
 # mySQL setting
 DATABASES = {
@@ -33,7 +32,7 @@ LANGUAGE_CODE = 'ja-jp'
 
 SECRET_KEY = 'django-insecure-2=j#c64aq8xu5@%^0@9k=q=ogo)!)7w5wcla(*i&u_xh2+2(6k'
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'fudosan-king.xyz']  # noqa
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'fudosan-king.xyz', 'cms.localhost']  # noqa
 
 BASE_URL = 'http://fudosan-king.xyz'
 

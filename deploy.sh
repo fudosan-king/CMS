98f2e5529b0ddc2ff919cdbb8882ae4f5431ff79
Truong Tam <tam@propolife.co.jp>
2021-08-24 10:01:06 +0700

    Update script sh

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 # rsync --delete -e  'ssh -i deployment/fdk-production.pem' -rlpcgz -v --exclude-from=excludes ./ ec2-user@54.238.184.234:/var/www/CMS/
 ssh database@fudosan-king.xyz -t 'cd ~/CMS && git pull --rebase'
 ssh database@fudosan-king.xyz -t 'cd ~/CMS/logknot && ./kill_uwsgi.sh'
-ssh database@fudosan-king.xyz -t 'cd ~/CMS/logknot && ./uwsgi.sh'
+ssh -t database@fudosan-king.xyz "sh -c 'cd ~/CMS/logknot; nohup ./uwsgi.sh > /dev/null 2>&1'"
--- logknot/kill_uwsgi.sh
+++ logknot/kill_uwsgi.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+
 sudo kill -9 $(ps aux | grep '../env/bin/uwsgi ../etc/production/uwsgi.ini' | awk '{print $2}')
